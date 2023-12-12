75a1519359704e78b1b388496e89bb36d7ff9722
rpm-1990 <reddypm1990@gmail.com>
2023-12-12 14:52:59 -0500

    commit_before_final

--- tests/Footer.test.js
+++ tests/Footer.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Footer from './Footer'; // Assuming the Footer component is in a separate file
+import Footer from '.components/Footer'; // Assuming the Footer component is in a separate file
 
 test('renders footer text correctly', () => {
-  render(<Footer />);
+  render(<Footer/>);
   const footerElement = screen.getByText('Website for final project testing purposes only submission by P M Reddy.');
   expect(footerElement).toBeInTheDocument();
-});;
+});
 
 
